3d612d278dbae26dfd3d52b849cc3cc8a5b7b12f
hwm <ccolacat@163.com>
2020-07-15 19:09:33 -0700

    Update usage documentation

---
+++ example/s2-cam/README.md
@@ -0,0 +1,21 @@
+# _ESP2-S2_CAM_
+
+![esp32-s2cam.jpg](docs/esp32-s2cam.jpg)
+
+## How to use
+
+Please burn it first and connect it to the wifi router.
+
+* Compile download
+
+```bash
+idf.py set-target esp32s2
+idf.py flash
+idf.py monitor
+```
+
+* Connect WiFi
+
+Development board wiring, power on.Connect to wifi using Lexin Esptouch, enter the wifi password and click to confirm the connection, as shown in the figure below:
+
+![1.png](docs/1.png)
---
+++ example/s2-cam/build/bootloader/CMakeFiles/3.5.1/CMakeASMCompiler.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_ASM_COMPILER "/home/aithinker-s2-16/.espressif/tools/xtensa-esp32s2-elf/esp-2020r1-8.2.0/xtensa-esp32s2-elf/bin/xtensa-esp32s2-elf-gcc")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/home/aithinker-s2-16/.espressif/tools/xtensa-esp32s2-elf/esp-2020r1-8.2.0/xtensa-esp32s2-elf/bin/xtensa-esp32s2-elf-ar")
+set(CMAKE_RANLIB "/home/aithinker-s2-16/.espressif/tools/xtensa-esp32s2-elf/esp-2020r1-8.2.0/xtensa-esp32s2-elf/bin/xtensa-esp32s2-elf-ranlib")
+set(CMAKE_LINKER "/home/aithinker-s2-16/.espressif/tools/xtensa-esp32s2-elf/esp-2020r1-8.2.0/xtensa-esp32s2-elf/bin/xtensa-esp32s2-elf-ld")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
